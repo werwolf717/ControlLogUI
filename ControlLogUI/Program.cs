182c013fa71d06862bf0f074770928b10d305d4d
s.balakin <s.balakin@radico.ru>
2021-06-08 17:47:43 +0300

    First commit

---
+++ ControlLogUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Qml.Net;
+using Qml.Net.Internal;
+using Qml.Net.Runtimes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ControlLogUI
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            
+            RuntimeManager.DiscoverOrDownloadSuitableQtRuntime();
+            
+            QQuickStyle.SetStyle("Material");
+
+            using (var app = new QGuiApplication(args))
+            {
+                using (var engine = new QQmlApplicationEngine())
+                {
+                    serviceCollection.AddSingleton<QCoreApplication>(app);
+                    engine.Load("UI/View/ControlLogUI.qml");
+                    app.Exec();
+                }
+            }
+        }
+    }
+}
